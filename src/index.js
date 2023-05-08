fb997af748feaaddceb3e0785d34d78911387e8b
PaoloVilardi <paolo.vilardi.tonic@gmail.com>
2023-05-08 18:23:42 +0200

    resolved slider, added the basic structure of other parts of front page and beginning of photo gallery

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  console.log(slides)
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
@@ -18,5 +17,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
 }

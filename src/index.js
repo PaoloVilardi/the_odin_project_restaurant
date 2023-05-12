f02670c4480b96eb74f71ba137e8ff49e515e3b3
PaoloVilardi <paolo.vilardi.tonic@gmail.com>
2023-05-12 11:50:00 +0200

    hover effect for the photo added, added the scrolling  option for the gallery and a few others better design  solution for a better UX/UI

--- src/index.js
+++ src/index.js
@@ -42,4 +42,63 @@ document.addEventListener('scroll', function(){
   } else {
     header.classList.remove('scrolled');
   }
-})
+});
+
+document.querySelectorAll('.photo').forEach(item =>{
+  
+  item.addEventListener('mousemove', (e) => {
+    // const readout = item.querySelector('.readout');
+    const { x, y } = item.getBoundingClientRect();
+    const itemStyle = window.getComputedStyle(item);
+    let itemWidth = parseInt(itemStyle.getPropertyValue('width'));
+    let itemHeight = parseInt(itemStyle.getPropertyValue('height'));
+    let computedX = Math.max(e.clientX - parseInt(x), 0);
+    let computedY = Math.max(e.clientY - parseInt(y), 0);
+    if(computedX < itemWidth && computedY < itemHeight){
+      item.style.setProperty('--x', computedX);
+      item.style.setProperty('--y', computedY);
+    }
+    // readout.innerText = `
+    // mouse X: ${e.clientX} mouse Y: ${e.clientY}
+    
+    // left edge: ${parseInt(x)} top edge: ${parseInt(y)}
+    
+    // shiny X: ${e.clientX - parseInt(x)} shiny Y: ${e.clientY - parseInt(y)}
+    // `;
+  });
+  let description = item.querySelector('.photo-description');
+  let subTitle = description.querySelector('.photo-subtitle');
+  // console.log(subTitle);
+  // description.querySelector('.photo-title').addEventListener('mouseover', (e) =>{
+  //   subTitle.classList.toggle('visible');
+  //   console.log("AAAAAAAAAAAAAAAAAAAAA");
+  // });
+  item.addEventListener('mouseenter', (e)=>{
+    subTitle.classList.toggle('visible');
+    console.log("AAAAAAAAAAAAAAAAAAAAA");
+  });
+  item.addEventListener('mouseleave', (e)=>{
+    subTitle.classList.toggle('visible');
+    console.log("AAAAAAAAAAAAAAAAAAAAA");
+  })
+});
+
+const photoList = document.querySelector('.photo-card-list');
+const backBtn = document.getElementById("back-btn");
+const nextBtn = document.getElementById("next-btn");
+const photo = photoList.querySelector('.photo');
+
+// photoList.addEventListener("wheel", (e)=>{
+//   e.preventDefault();
+//   photoList.scrollLeft += e.deltaY;
+// });
+
+nextBtn.addEventListener('click', ()=>{
+  photoList.style.scrollBehavior = "smooth";
+  photoList.scrollLeft += photo.offsetWidth;
+});
+backBtn.addEventListener('click', ()=>{
+  photoList.style.scrollBehavior = "smooth";
+  photoList.scrollLeft -= photo.offsetWidth;
+});
+
